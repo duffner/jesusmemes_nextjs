5ad303caeb4b361e4fbea790a0c8849721f3c27b
Robert Duffner <robert@findcanary.com>
2023-02-04 23:17:58 -0600

    Cross Origin Embed/Opener and HSTS

--- next.config.js
+++ next.config.js
@@ -3,4 +3,26 @@ module.exports = {
     formats: ['image/avif', 'image/webp'],
     domains: ['res.cloudinary.com'],
   },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "credentialless",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains; preload",
+          },
+        ],
+      },
+    ]
+  },
 }
